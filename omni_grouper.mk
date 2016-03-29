5490615c1193c9a84f97a8e2a24b4a3cf81d4979
André Pinela <sheffzor@gmail.com>
2016-03-29 14:39:43 +0100

    omnirom: bring up
    Change-Id: I8d0140cc54b6b478e2fe3af9feea88cb0b5355d3
    Signed-off-by: André Pinela <sheffzor@gmail.com>

---
+++ omni_grouper.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for grouper hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common_tablet.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/asus/grouper/aosp_grouper.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := omni_grouper
+PRODUCT_DEVICE := grouper
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# TWRP
+TW_THEME := portrait_hdpi
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_USB_STORAGE := false
+TW_INCLUDE_L_CRYPTO := true
+PRODUCT_COPY_FILES += \
+	device/asus/grouper/twrp.fstab:recovery/root/etc/twrp.fstab
