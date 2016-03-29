b5e70e756896123ef8e6acc06c9ad93318f3ee51
André Pinela <sheffzor@gmail.com>
2016-03-29 14:41:57 +0100

    cyanogenmod: bring up
    Change-Id: Ice7c1d3993cc5524492b3461b290709d66202bad
    Signed-off-by: André Pinela <sheffzor@gmail.com>

---
+++ cm.mk
@@ -0,0 +1,25 @@
+# Release name
+PRODUCT_RELEASE_NAME := nakasi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/grouper/aosp_grouper.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := grouper
+PRODUCT_NAME := cm_grouper
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:5.1/LMY47D/1743759:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.1 LMY47D 1743759 release-keys"
