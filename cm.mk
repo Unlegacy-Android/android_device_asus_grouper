e8bd06dfeddf3bbdac95f1feaea4cd78ee1b893f
André Pinela <sheffzor@gmail.com>
2016-08-17 04:46:05 +0100

    optimize rom space and include Snap camera
    Change-Id: I6c1991a19b7ec34f8472f8706571269d32718cb9
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- cm.mk
+++ cm.mk
@@ -6,13 +6,21 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/common_mini_tablet_wifionly.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/grouper/aosp_grouper.mk)
+
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
+# Themes
+PRODUCT_PACKAGES += \
+    HexoLibre
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grouper
