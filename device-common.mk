655553c9846c41a4838ad7c2b4d9a21cebe5a793
D. Andrei Măceș <dmaces@nd.edu>
2016-09-13 22:27:48 -0400

    grouper: Build Launcher3
    Change-Id: I509adc3a3afeb9a3c422d5a843b50f99842dc7b1

--- device-common.mk
+++ device-common.mk
@@ -117,6 +117,10 @@ PRODUCT_PACKAGES += \
     Terminal
 endif
 
+# Launcher3
+PRODUCT_PACKAGES += \
+    Launcher3
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
