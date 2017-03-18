9112c44d2e5aed0ebfffa15443cfae4dc9d4e988
D. Andrei Măceș <dmaces@nd.edu>
2017-03-18 14:45:19 -0400

    common: Switch to hardware/nvidia/tegra3
    Change-Id: I657ef0a3502ef944a9427c768c9c959d3cbdcd0c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,13 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_BOARD_PLATFORM := tegra3
-
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a9
+include hardware/nvidia/tegra3/BoardConfigCommon.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := grouper
 
@@ -73,5 +67,4 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/asus/grouper/bluetooth
 BOARD_HAL_STATIC_LIBRARIES := libdumpstate.grouper
 
 # Security
-BOARD_USES_SECURE_SERVICES := true
 BOARD_SEPOLICY_DIRS += device/asus/grouper/sepolicy
