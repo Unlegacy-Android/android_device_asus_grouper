56e2a5d6cfcb8f1a6ab0da9fdc3413c0fcd3bcf9
Daniel Jarai <jaraidaniel@gmail.com>
2018-01-04 12:02:53 +0100

    Remove releasetools extensions
    We don't want to flash a new bootloader or radio firmware as part of
    OTA updates.
    
    Change-Id: I07c2c479fa49ed054f49bd2d7adda0930812ad8c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_RELEASETOOLS_EXTENSIONS := device/asus/grouper
-
 -include vendor/asus/grouper/BoardConfigVendor.mk
 include device/asus/grouper/BoardConfigCommon.mk
 
